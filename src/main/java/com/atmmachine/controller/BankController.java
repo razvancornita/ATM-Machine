6bce3a428c4ec96c2669727fc93b466203f80fd0
razvancornita <rrazvan.cornita@gmail.com>
2020-11-20 23:15:06 +0200

    - changed endpoint methods

--- src/main/java/com/atmmachine/controller/BankController.java
+++ src/main/java/com/atmmachine/controller/BankController.java
@@ -5,9 +5,7 @@ import com.atmmachine.service.BankService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -18,8 +16,13 @@ public class BankController {
     @Autowired
     BankService bankService;
 
-    @PostMapping(value = "/atmOperation/{cardId}")
+    @GetMapping(value = "/atmOperation/{cardId}")
     public ResponseEntity<BigDecimal> getAmount(@PathVariable("cardId") String cardId) {
+        return null;
+    }
+
+    @DeleteMapping(value = "/atmOperation/{cardId}")
+    public ResponseEntity<String> deleteOperation(@PathVariable("cardId") String cardId) {
         return null;
     }
 
@@ -28,12 +31,7 @@ public class BankController {
         return null;
     }
 
-    @PostMapping(value = "/atmOperation/{cardId}")
-    public ResponseEntity<String> deleteOperation(@PathVariable("cardId") String cardId) {
-        return null;
-    }
-
-    @PostMapping(value = "/atmOperation")
+    @PutMapping(value = "/atmOperation")
     public ResponseEntity changePin(@RequestBody BankOperationRequest request) {
         return null;
     }
