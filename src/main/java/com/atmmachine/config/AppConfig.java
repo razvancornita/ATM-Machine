34f7a8409c6503714555abbe4ad1de61fa2a1908
razvancornita <rrazvan.cornita@gmail.com>
2020-11-21 00:17:47 +0200

    - added database config (JdbcTemplate Bean) - added commons-dbcp2 dependency

---
+++ src/main/java/com/atmmachine/config/AppConfig.java
@@ -0,0 +1,42 @@
+package com.atmmachine.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dataSourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dataSourcePassword;
+
+    @Value("${spring.datasource.driverClassName}")
+    private String dataSourceDriverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(dataSourceUrl);
+        dataSource.setUsername(dataSourceUsername);
+        dataSource.setPassword(dataSourcePassword);
+        dataSource.setDriverClassName(dataSourceDriverClassName);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
