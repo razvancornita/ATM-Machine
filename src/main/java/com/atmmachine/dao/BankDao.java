a4a5c1162959adfe45f832d9bde05de6c4bb4e89
razvancornita <rrazvan.cornita@gmail.com>
2020-11-21 11:50:55 +0200

    - added getCard database method - added Card model

--- src/main/java/com/atmmachine/dao/BankDao.java
+++ src/main/java/com/atmmachine/dao/BankDao.java
@@ -1,16 +1,14 @@
 package com.atmmachine.dao;
 
 import com.atmmachine.model.BankAccount;
+import com.atmmachine.model.Card;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
 
 @Component
 @Slf4j
@@ -22,6 +20,9 @@ public class BankDao {
     @Value("${atmMachine.getAccount}")
     private String getAccountQuery;
 
+    @Value("${atmMachine.getCard}")
+    private String getCardQuery;
+
     @Value("${atmMachine.getAccountId}")
     private String getAccountIdQuery;
 
@@ -32,11 +33,30 @@ public class BankDao {
             BankAccount bankAccount = jdbcTemplate.queryForObject(getAccountQuery, params, new BeanPropertyRowMapper<>(BankAccount.class));
             log.debug("got bank account = {} for id = {}", bankAccount, bankAccountId);
             return bankAccount;
+        } catch(EmptyResultDataAccessException e) {
+            log.error("no account found for id = {}", bankAccountId, e);
+            throw e;
         } catch (Exception e) {
             log.error("error getting bank account for id = {}", bankAccountId, e);
             throw e;
         }
 
+    }
+
+    public Card getCard(int cardId) {
+        log.debug("getting card for cardId = {}", cardId);
+        try {
+            Object[] params = {cardId};
+            Card card = jdbcTemplate.queryForObject(getCardQuery, params, new BeanPropertyRowMapper<>(Card.class));
+            log.debug("got card = {} for id = {}", card, cardId);
+            return card;
+        } catch(EmptyResultDataAccessException e) {
+            log.error("no card found for id = {}", cardId, e);
+            throw e;
+        } catch (Exception e) {
+            log.error("error getting card for id = {}", cardId, e);
+            throw e;
+        }
     }
 
     public int getAccountIdForCardId(int cardId) {
---
+++ src/main/java/com/atmmachine/model/Card.java
@@ -0,0 +1,14 @@
+package com.atmmachine.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class Card {
+    private int id;
+    private int bankAccountId;
+    private int pin;
+}
