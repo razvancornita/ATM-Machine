775e208d1b9dd0cb0e072a4024bf0d56cdced765
razvancornita <rrazvan.cornita@gmail.com>
2020-11-22 23:32:53 +0200

    - added testing configuration for DAO

--- src/test/java/com/atmmachine/dao/BankDaoTest.java
+++ src/test/java/com/atmmachine/dao/BankDaoTest.java
@@ -2,7 +2,9 @@ package com.atmmachine.dao;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,4 +13,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @Slf4j
 @ActiveProfiles("unit_test")
 public class BankDaoTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    BankDao bankDao;
 }
