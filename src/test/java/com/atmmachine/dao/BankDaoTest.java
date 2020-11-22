86a005f17f894aedf89520a80d98f468c7c7fe60
razvancornita <rrazvan.cornita@gmail.com>
2020-11-22 23:35:42 +0200

    - commented annotation

--- src/test/java/com/atmmachine/dao/BankDaoTest.java
+++ src/test/java/com/atmmachine/dao/BankDaoTest.java
@@ -1,14 +1,12 @@
 package com.atmmachine.dao;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @Slf4j
 @ActiveProfiles("unit_test")
