1d22638353fc3817185065179e2c669a7e345991
razvancornita <rrazvan.cornita@gmail.com>
2020-11-22 09:42:57 +0200

    - added test for happy flow of getBalance endpoint

--- src/test/java/com/atmmachine/controller/BankControllerTest.java
+++ src/test/java/com/atmmachine/controller/BankControllerTest.java
@@ -1,6 +1,10 @@
 package com.atmmachine.controller;
 
 import com.atmmachine.constants.BankConstants;
+import com.atmmachine.model.BankAccount;
+import com.atmmachine.model.Currency;
+import com.atmmachine.service.AccountService;
+import com.atmmachine.service.BankService;
 import com.atmmachine.service.CardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +14,50 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.nio.file.AccessDeniedException;
 
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BankController.class)
 class BankControllerTest {
 
+    private static final int TEST_CARD_ID = 4657321;
+
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
+    private BankService bankService;
+
+    @MockBean
+    private AccountService accountService;
+
+    @MockBean
     private CardService cardService;
+
+    @Test
+    void testGetBalanceHappyFlow() throws Exception {
+        when(cardService.getCardId()).thenReturn(TEST_CARD_ID);
+
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setBalance(0.00);
+        bankAccount.setCurrency(Currency.RON);
+
+        when(accountService.getAccountByCardId(eq(TEST_CARD_ID))).thenReturn(bankAccount);
+
+        this.mockMvc
+                .perform(get("/atmOperation"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(bankAccount.getBalance() + " "
+                        + bankAccount.getCurrency() + " left")
+                );
+    }
+
 
     @Test
     void testGetBalanceReturnsUnauthorized() throws Exception {
